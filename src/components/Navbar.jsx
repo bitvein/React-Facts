db81a1737723a7204d2c55a2b8e0e4b0b5673ef1
Fatjon Myftaraj <fatjonmyftaraj1@gmail.com>
2025-07-05 17:55:43 +0200

    Fix image path for deployment

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,12 @@
+import logo from "/src/images/react-logo.png";
+
 export default function Navbar() {
   return (
     <>
       {/* <h1>Navbar goes here</h1> */}
       <header>
         <nav>
-          <img src="src/images/react-logo.png" alt="react logo"/>
+          <img src={logo} alt="react logo" />
           <span>ReactFacts</span>
         </nav>
       </header>

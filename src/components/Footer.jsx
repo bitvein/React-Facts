d174b8c6d883a2713b88ef1a5f313f41ec8ca4fe
Fatjon Myftaraj <fatjonmyftaraj1@gmail.com>
2025-07-05 16:33:55 +0200

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,5 @@
+export default function Footer() {
+  return  <footer>
+  <small>© 2025 Myftaraj development. All rights reserved.</small>
+  </footer>
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+  return (
+    <>
+      {/* <h1>Navbar goes here</h1> */}
+      <header>
+        <nav>
+          <img src="src/images/react-logo.png" alt="react logo"/>
+          <span>ReactFacts</span>
+        </nav>
+      </header>
+    </>
+  );
+}
